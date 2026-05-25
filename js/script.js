064d62fb3826a862eea2dcd33e02f35cd39ead29
Eduard <118971330+EduardUrakov@users.noreply.github.com>
2026-05-25 20:05:47 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const percents = document.querySelectorAll('.skills__level-percent'),
+    lines = document.querySelectorAll('.skills__level-line');
+
+percents.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
